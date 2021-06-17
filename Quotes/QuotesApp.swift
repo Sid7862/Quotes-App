0d574ee8df9d8577ff42011b13c090ffdab55e92
dheeraj.iosdev <dheeraj@vumonic.com>
2021-06-17 12:50:18 +0530

    Initial Commit

---
+++ Quotes/QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesApp.swift
+//  Quotes
+//
+//  Created by Dheeraj Kumar Sharma on 17/06/21.
+//
+
+import SwiftUI
+
+@main
+struct QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
